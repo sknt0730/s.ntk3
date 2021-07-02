b047b0e8c0329718cf7f6aa817af5a549e3659d6
sknt0730 <zm19568@josai.ac.jp>
2021-07-02 11:30:20 +0000

    07021

--- game.js
+++ game.js
@@ -10,6 +10,7 @@ class Game {
         document.body.appendChild( this.canvas );
         this.canvas.width = width || 320;
         this.canvas.height = height || 320;
+        
         this.objs = [];
     }
 
@@ -22,14 +23,15 @@ class Game {
         ctx.fillStyle="#000000" ;
         ctx.fillRect( 0,0,this.canvas.width,this.canvas.height );
 
-        for (let i=0; i<this.objs.length; i++ ) {
+        for ( let i=0; i<this.objs.length; i++ ) {
             this.objs[i].update( this.canvas );
         }
-
+       
         requestAnimationFrame( this._mainLoop.bind( this ));
     }
 
     add( obj ) {
         this.objs.push( obj );
     }
+    
 }
--- main.js
+++ main.js
@@ -2,8 +2,6 @@
 
 addEventListener('load',()=> {
     const game =new Game(640,480);
-
-    const inu = new Sprite( 'image/$PW-DogRetriever01.png')
-    game.add( inu );
+    
     game.start();
 });
