f61e9590506b0332954f6d4c2579d296f33148b9
sknt0730 <zm19568@josai.ac.jp>
2021-07-04 14:13:09 +0000

    07021

--- main.js
+++ main.js
@@ -3,8 +3,7 @@
 addEventListener('load',()=> {
     const game =new Game(640,480);
 
-    const game = new Sprite( 'image/$PW-DogRetriever01.png');
-    game.add( inu );
+    
     
     game.start();
 });
